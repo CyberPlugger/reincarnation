f5293f54fe4f4ff8a51fd73a89ab321739751b42
CyberPlugger <ilikemyemail009@mail.ru>
2026-04-15 17:14:49 +0300

    i added some library stuff

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
-from reincarnation.main import *
+"""just some library stuff
+look in reincarnation.main to see more desc"""
+
+from main import *
+import main
+import sys
+from os import name
+
+__all__ = ()
+
+imports = ['main', 'name']
+main_exc = type('MainException', (Exception,), {})
+sys.modules['reincarnation'] = main
+
+if hasattr(main, '__all__'):
+    __all__ = main.__all__
+
+NT = 'nt'
+if name != NT:
+    raise main_exc('Not allowed on this platform')
+
+if __name__ == '__main__':
+    print(sys.modules['reincarnation'])
