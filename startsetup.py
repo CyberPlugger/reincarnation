41a29f7703453027c57110906e64f683837d56f0
CyberPlugger <ilikemyemail009@mail.ru>
2026-04-16 22:15:19 +0300

    Delete startsetup.py

--- startsetup.py
+++
@@ -1,9 +0,0 @@
-from os import system
-code = '''
-pip install twine
-python setup.py sdist bdist_wheel
-twine upload dist/*
-'''.split('\n')
-if __name__ == '__main__':
-    for command in code:
-        system(command)
